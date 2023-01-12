e33cf55019df479ae8dbfda168fa0d6460928548
Muhammad Arshad K <muhammadarshad8935@gmail.com>
2023-01-13 01:38:50 +0530

    modified the working

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 function App() {
   const [toDos,setToDos] = useState([])
   const [toDo,setToDo] =useState("")
+  const [filter,setFilter]=useState("")
  
   return (
     <div className="app">
@@ -12,61 +13,85 @@ function App() {
       </div>
       <div className="subHeading">
         <br />
-        
         <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
       </div>          
       <div className="input"> 
-        <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={()=>setToDos([...toDos,{ key:Date.now() ,id:Date.now() ,text:toDo,status:false,delete:false}])} className="fas fa-plus"></i>
+        <input onChange={(e)=> setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setToDos([...toDos,{ key:Date.now() ,id:Date.now() ,text:toDo,status:false,delete:false}]) } className="fas fa-plus"></i>
       </div>
-      <div className="todos">
-        { toDos.map((obj)=>{
+        
+      <div className='buttons'>
+          <button onClick={ ()=>setFilter("all")} >All tasks</button>
+          <button onClick={ ()=>setFilter("pending")} >in Progres</button>
+          <button onClick={ ()=>setFilter("done")} >Task Done</button>
+        </div>
+       <div className="todos">   
+        {toDos.map((obj,index)=>{       
+          if(obj.text===""){
+            return null;
+          }else{
+            if(filter===""||filter==="all"){
+             return(
+              <div key={index} className="todo">
+              <div className="left">
+                <input onChange={(e)=>{
+                  setToDos(toDos.filter(data => {
+                    if(data.id === obj.id){
+                      data.status = e.target.checked
+                    }
+                   return data
+                  }))
+                }} 
+                value={obj.status} type="checkbox" name="" id="" checked={obj.status} />
+                <p>{obj.status===true?<s>{obj.text}</s>:obj.text}</p>
+              </div>
+              <div className="right">
+                <i onClick={() => {
+                 return setToDos(  
+                 toDos.filter((obj3) => obj3.id !== obj.id)    
+                 );
+                }} className="fas fa-times"></i>
+              </div>
+            </div>
+             )
+            }else if(filter==="done" || filter ==="pending"){
+              const status = filter ==="done"? true:false;
+              if(obj.status === status){
+                return (
+                  <div key={index} className="todo">
+                    <div className='left'>
+                        <input onChange={(e)=>{
+                          setToDos(toDos.filter(obj2=>{
+                            if(obj.id === obj2.id ){
+                            obj2.status=e.target.checked
+                            }
+                            return obj2;
+                          }))
+                        }} name="" id ="" checked={obj.status} type="checkbox" />
+                        <p>{obj.status === true ? <s>{obj.text}</s> : obj.text}</p>
+                    </div>
+                    <div className="right">
+                        <i
+                          onClick={() => {
+                            return setToDos(
+                              toDos.filter((obj3) => obj3.id !== obj.id)
+                            );
+                          }}
+                          className="fas fa-times"></i>
+                      </div>
+                  </div>
+                )
+              }
+            }else{
 
-          return ( 
-        <div className="todo">
-          <div className="left">
-            <input onChange={(e)=>{
-              console.log(e.target.checked);
-              console.log(obj);
-              setToDos(toDos.filter(obj2 => {
-                if(obj2.id === obj.id){
-                  obj2.status = e.target.checked
-                }
-                return obj2
-              }))
-            }} value={obj.status} type="checkbox" name="" id="" />
-            <p>{obj.text}</p>
-          </div>
-          <div className="right">
-            <i onClick={()=>{
-                setToDos(toDos.filter(obj2 => {
-                  let temp;
-                  if (obj2.id != obj.id){
-                    temp = obj2
-                  }
-                  return temp;
-                }));
-              }} className="fas fa-times"></i>
-          </div>
-        </div>
-        )  }) }
-        <div className='buttons'>
-          <button onClick={} >All tasks</button>
-          <button>Task Done</button>
-          <button>in Progres</button>
-        </div>
-        {
-          toDos.map((obj)=>{
-            if(obj){
-              return(<li>{obj.text}</li> )
             }
-            return null
-          })      
+            return null;
           }
-          
+        })
+        }      
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
